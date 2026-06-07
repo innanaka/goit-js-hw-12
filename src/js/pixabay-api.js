35a7bce99a66d47d35b82070ed396495923a2dc3
innanaka <gordiienkoinna30@gmail.com>
2026-06-07 16:00:25 +0300

    part 2 fix

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,12 @@
+import axios from 'axios'; // 
+
 const API_KEY = '56116338-e065c09dde8a76b57de900d0e';
 
 export async function getImagesByQuery(query, page) {
-  
+
   const url = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=15`;
 
-  const response = await fetch(url);
   
-  if (!response.ok) {
-    throw new Error('Failed to fetch images');
-  }
-
-  const data = await response.json();
-  return data;
+  const response = await axios.get(url);
+  return response.data;
 }
